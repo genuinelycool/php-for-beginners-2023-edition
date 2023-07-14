3e60b9e73041fc67f8b5d77ed148dfa9b0e25c72
genuinelycool <genuinelycool@gmail.com>
2023-07-14 10:10:06 +0545

    functions and filters

---
+++ functions-and-filters.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Demo | Functions and Filters</title>
+</head>
+<body>
+    <?php
+        $books = [
+            [
+                'name' => 'book 1',
+                'author' => 'author 1',
+                'releaseYear' => 1968,
+                'purchaseUrl' => 'http://example1.com'
+            ],[
+                'name' => 'book 2',
+                'author' => 'author 2',
+                'releaseYear' => 2010,
+                'purchaseUrl' => 'http://example1.com'
+            ],
+            [
+                'name' => 'book 3',
+                'author' => 'author 2',
+                'releaseYear' => 2011,
+                'purchaseUrl' => 'http://example3.com'
+            ]
+        ];
+
+        function filterByAuthor($books, $author) {
+            $filteredBooks = [];
+
+            foreach ($books as $book) {
+                if ($book['author'] === $author) {
+                    $filteredBooks[] = $book;
+                }
+            }
+
+            return $filteredBooks;
+        }
+
+    ?>
+    
+    <ul>
+        <?php foreach (filterByAuthor($books, 'author 2') as $book) : ?>
+                <li>
+                    <a href="<?= $book['purchaseUrl']; ?>">
+                        <?= $book['name']; ?> (<?= $book['releaseYear']; ?>) - By <?= $book['author']; ?>
+                    </a>            
+                </li>
+        <?php endforeach ?>
+    </ul>
+    
+</body>
+</html>
