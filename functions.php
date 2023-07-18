59467e2fe04cc0feecd1b4851dd9259bd2a28b24
genuinelycool <genuinelycool@gmail.com>
2023-07-18 12:45:21 +0545

    code cleanup, remove comments

--- functions.php
+++ functions.php
@@ -9,19 +9,6 @@ function dd($value)
     die();
 }
 
-// dd($_SERVER['REQUEST_URI']);
-// dd($heading);
-
-// echo $_SERVER['REQUEST_URI'];
-
-// if ($_SERVER['REQUEST_URI'] === '/') {
-//     echo 'bg-gray-900 text-white';
-// } else {
-//     'text-gray-300';
-// }
-
-// echo $_SERVER['REQUEST_URI'] === '/' ? 'bg-gray-900 text-white' : 'text-gray-300';
-
 function urlIs($value) {
     return $_SERVER['REQUEST_URI'] === $value;
 }
