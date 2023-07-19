91eb9bb78fb59db1645bc2de443c7fe448b5e154
genuinelycool <genuinelycool@gmail.com>
2023-07-19 12:30:00 +0545

    pdo first steps

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
 <?php
 
 require 'functions.php';
-require 'router.php';
+// require 'router.php';
+
+// connect to our MySQL database.
+// $dsn = "mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4";
+$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+
+// $pdo = new PDO($dsn, 'root');
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("select * from posts");
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+// dd($posts);
+
+foreach ($posts as $post) {
+    echo "<li>" . $post['title'] ."</li>";
+}
