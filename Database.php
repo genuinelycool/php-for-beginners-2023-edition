7cc0b6d27647a0d602ac0fd98c02e239767cf9b1
genuinelycool <genuinelycool@gmail.com>
2023-07-19 22:58:11 +0545

    environments and configuration

--- Database.php
+++ Database.php
@@ -4,11 +4,26 @@
 class Database {
     public $connection;
 
-    public function __construct()
+    // public function __construct($config)
+    public function __construct($config, $username = 'root', $password = '')
     {
-        $dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+        
 
-        $this->connection = new PDO($dsn);
+        // dd(http_build_query($config, '', ';'));
+        // dd('mysql:' . http_build_query($config, '', ';'));  // string(59) "mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4"
+
+        // http_build_query($data);    // example.com?host=localhost$port=3306&dbname=myapp&charset=utf8mb4
+        // http_build_query($config, '', ';');    // host=localhost;port=3306;dbname=myapp;charset=utf8mb4
+
+        // $dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+        // $dsn = "mysql:host={$config['host']};port={$config['port']};dbname={$config['dbname']};charset={$config['charset']}";
+
+        $dsn = 'mysql:' . http_build_query($config, '', ';');
+
+        // $this->connection = new PDO($dsn, 'root', '', [
+        $this->connection = new PDO($dsn, $username, $password, [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
     }
 
     public function query($query)
--- index.php
+++ index.php
@@ -3,11 +3,22 @@
 require 'functions.php';
 require 'Database.php';
 
+// $config = [
+//     'host' => 'localhost',
+//     'port' => 3306,
+//     'dbname' => 'myapp',
+//     'charset' => 'utf8mb4'
+// ];
 
-$db = new Database();
+// $config = require('config.php');
+$config = require'config.php';
+
+// $db = new Database($config);
+$db = new Database($config['database']);
 
 // $post = $db->query("select * from posts where id = 1")->fetch(PDO::FETCH_ASSOC);
-$posts = $db->query("select * from posts where id = 1")->fetchAll(PDO::FETCH_ASSOC);
+// $posts = $db->query("select * from posts where id = 1")->fetchAll();
+$posts = $db->query("select * from posts")->fetchAll();
 
 dd($posts);
 
