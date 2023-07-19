33368ed327b4125fe6e70a01384447cbaeb0d371
genuinelycool <genuinelycool@gmail.com>
2023-07-20 03:02:10 +0545

    code cleanup, remove comments

--- index.php
+++ index.php
@@ -7,20 +7,10 @@ $config = require'config.php';
 
 $db = new Database($config['database']);
 
-// dd($_GET);
-// dd($_GET['id']);
-
 $id = $_GET['id'];
-// $query = "select * from posts where id = {$id}";     // never inline the variable (ie. user data) into a query string.
-// $query = "select * from posts where id = ?";
-$query = "select * from posts where id = :id";      // key
 
-// dd($query);
+$query = "select * from posts where id = :id";
 
-// $posts = $db->query("select * from posts")->fetchAll();
-// $posts = $db->query($query)->fetch();
-// $posts = $db->query($query, [$id])->fetch();
-// $posts = $db->query($query, ['id' => $id])->fetch();     // pass in a associative array. where key begins with colon(:) or no colon.
 $posts = $db->query($query, [':id' => $id])->fetch();
 
 dd($posts);
