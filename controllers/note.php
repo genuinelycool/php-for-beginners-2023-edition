dd7c8e5db70f4e77e9090d9a769edb870c0b8cde
genuinelycool <genuinelycool@gmail.com>
2023-07-22 11:07:48 +0545

    intro to Authorization

--- controllers/note.php
+++ controllers/note.php
@@ -4,7 +4,25 @@ $config = require'config.php';
 $db = new Database($config['database']);
 
 $heading = 'Note';
+$currentUserId = 1;
 
-$note = $db->query('SELECT * FROM notes WHERE id = :id', ['id' => $_GET['id']])->fetch();
+// $note = $db->query('SELECT * FROM notes WHERE id = :id', ['id' => $_GET['id']])->fetch();
+// $note = $db->query('SELECT * FROM notes WHERE user_id = :user and id = :id', [
+$note = $db->query('SELECT * FROM notes WHERE id = :id', [
+    // 'user' => 1,
+    'id' => $_GET['id']
+])->fetch();
+
+if (! $note) {
+    abort();
+}
+
+// $currentUserId = 1;
+// $forbidden = 403;
+
+if ($note['user_id'] !== $currentUserId) {
+    // abort(403);
+    abort(Response::FORBIDDEN);
+}
 
 require "views/note.view.php";
