8a190aad06671fcda7c6fa04cb8a865eff97be58
genuinelycool <genuinelycool@gmail.com>
2023-07-22 11:15:46 +0545

    code cleanup, remove comments

--- controllers/note.php
+++ controllers/note.php
@@ -6,10 +6,7 @@ $db = new Database($config['database']);
 $heading = 'Note';
 $currentUserId = 1;
 
-// $note = $db->query('SELECT * FROM notes WHERE id = :id', ['id' => $_GET['id']])->fetch();
-// $note = $db->query('SELECT * FROM notes WHERE user_id = :user and id = :id', [
 $note = $db->query('SELECT * FROM notes WHERE id = :id', [
-    // 'user' => 1,
     'id' => $_GET['id']
 ])->fetch();
 
@@ -17,11 +14,7 @@ if (! $note) {
     abort();
 }
 
-// $currentUserId = 1;
-// $forbidden = 403;
-
 if ($note['user_id'] !== $currentUserId) {
-    // abort(403);
     abort(Response::FORBIDDEN);
 }
 
