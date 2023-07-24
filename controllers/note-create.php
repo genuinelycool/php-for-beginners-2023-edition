930a87f86f34a69634d85a293a3fdeb2058e196a
genuinelycool <genuinelycool@gmail.com>
2023-07-24 21:20:10 +0545

    escape untrusted input

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,10 +1,17 @@
 <?php
 
+$config = require'config.php';
+$db = new Database($config['database']);
+
 $heading = 'Create Note';
 
 // dd($_SERVER);
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    dd($_POST);
+    // dd($_POST);
+    $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
+        'body' => $_POST['body'],
+        'user_id' => 1
+    ]);
 }
 
 require 'views/note-create.view.php';
