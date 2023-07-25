3ad1dadf07b1babbff3e76da48294fe415126cb5
genuinelycool <genuinelycool@gmail.com>
2023-07-25 14:03:47 +0545

    code cleanup, remove comments

--- Validator.php
+++ Validator.php
@@ -2,22 +2,10 @@
 
 class Validator
 {
-    // public function string($value, $min = 1, $max = INF)
     public static function string($value, $min = 1, $max = INF)
     {
         $value = trim($value);
-
-        // return strlen($_POST['body']) === 0;
-        // return strlen(trim($value)) === 0;
-
+        
         return strlen($value) >= $min && strlen($value) <= $max;
     }
-
-    // public static function email($value)
-    // {
-    //     // Validator::email('a@a.com');
-    //     // Validator::email('dsafdasfsfdsf');
-
-    //     return filter_var($value, FILTER_VALIDATE_EMAIL);
-    // }
 }
--- controllers/note-create.php
+++ controllers/note-create.php
@@ -3,33 +3,17 @@
 require 'Validator.php';
 
 $config = require 'config.php';
-// $config = require'config.php';
 
 $db = new Database($config['database']);
 
 $heading = 'Create Note';
 
-// dd(Validator::email('a@a.com'));
-// if (! Validator::email('dasfdaf')) {
-// // if (! Validator::email('a@a.com')) {
-//     dd('that is not a valid email');
-// }
-
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $errors = [];
 
-    // $validator = new Validator();
-
-    // if (strlen($_POST['body']) === 0) {
-    // if ($validator->string($_POST['body'])) {
-    // if (! $validator->string($_POST['body'], 1, 1000)) {
     if (! Validator::string($_POST['body'], 1, 1000)) {
         $errors['body'] = 'A body of no more than 1,000 is required';
     }
-
-    // if (strlen($_POST['body']) > 1000) {
-    //     $errors['body'] = 'A body cannot be more than 1,000 characters.';
-    // }
 
     if (empty($errors)) {
         $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
