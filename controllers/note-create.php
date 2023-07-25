4ef5b57f4566235ee4efd0447933349eb7466ca7
genuinelycool <genuinelycool@gmail.com>
2023-07-25 14:02:18 +0545

    extract a simple validator class

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,20 +1,35 @@
 <?php
 
-$config = require'config.php';
+require 'Validator.php';
+
+$config = require 'config.php';
+// $config = require'config.php';
+
 $db = new Database($config['database']);
 
 $heading = 'Create Note';
 
+// dd(Validator::email('a@a.com'));
+// if (! Validator::email('dasfdaf')) {
+// // if (! Validator::email('a@a.com')) {
+//     dd('that is not a valid email');
+// }
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $errors = [];
 
-    if (strlen($_POST['body']) === 0) {
-        $errors['body'] = 'A body is required';
+    // $validator = new Validator();
+
+    // if (strlen($_POST['body']) === 0) {
+    // if ($validator->string($_POST['body'])) {
+    // if (! $validator->string($_POST['body'], 1, 1000)) {
+    if (! Validator::string($_POST['body'], 1, 1000)) {
+        $errors['body'] = 'A body of no more than 1,000 is required';
     }
 
-    if (strlen($_POST['body']) > 1000) {
-        $errors['body'] = 'A body cannot be more than 1,000 characters.';
-    }
+    // if (strlen($_POST['body']) > 1000) {
+    //     $errors['body'] = 'A body cannot be more than 1,000 characters.';
+    // }
 
     if (empty($errors)) {
         $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
