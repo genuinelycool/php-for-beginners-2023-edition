88894535170eda469683cdbeeb65dbb2af3f124f
genuinelycool <genuinelycool@gmail.com>
2023-08-02 07:44:42 +0545

    handle multiple request methods

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -4,16 +4,36 @@ use Core\Database;
 
 $config = require base_path('config.php');
 $db = new Database($config['database']);
+// $currentUserId = 1;
+$currentUserId = 25;
 
-$currentUserId = 1;
+// dd($_SERVER);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $note = $db->query('SELECT * FROM notes WHERE id = :id', [
+        'id' => $_GET['id']
+    ])->findOrFail();
 
-$note = $db->query('SELECT * FROM notes WHERE id = :id', [
-    'id' => $_GET['id']
-])->findOrFail();
+    authorize($note['user_id'] === $currentUserId);
+    // form was submitted. delete the current note.
+    // dd($_POST);
+    $db->query('delete from notes where id = :id', [
+        'id' => $_GET['id']
+    ]);
 
-authorize($note['user_id'] === $currentUserId);
+    header('Location: /notes');
+    exit();
+} else {
 
-view("notes/show.view.php", [
-    'heading' => 'Note',
-    'note' => $note
-]);
+    // $currentUserId = 1;
+
+    $note = $db->query('SELECT * FROM notes WHERE id = :id', [
+        'id' => $_GET['id']
+    ])->findOrFail();
+
+    authorize($note['user_id'] === $currentUserId);
+
+    view("notes/show.view.php", [
+        'heading' => 'Note',
+        'note' => $note
+    ]);
+}
